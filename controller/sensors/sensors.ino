cec45d68fd85259924b9542a40a5974a3418c18e
Sebastião Sousa Soares <sebastiaosoares145@gmail.com>
2023-10-22 14:29:46 -0300

    update html

---
+++ controller/sensors/sensors.ino
@@ -0,0 +1,16 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "PLUGNET-22355-2.4";
+const char* password = "82ou8jt3";
+const char* serverAddress = "http://192.168.0.112";
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ controller/server.py
@@ -0,0 +1,8 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return send_from_directory('../app', 'index.html')
