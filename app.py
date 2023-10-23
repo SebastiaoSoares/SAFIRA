e0fa70e98b4b7cd293168bf15c1ac5a563c96207
Sebastião Sousa Soares <sebastiaosoares145@gmail.com>
2023-10-22 21:29:28 -0300

    update all

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    fResult = "safe"
+    tResult = "no_safe"
+    uResult = "error"
+    return render_template('index.html', fResult=fResult, tResult=tResult, uResult=uResult)
+    # x_results = ["% of precision", "status (safe or no_safe)", "result text"]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
