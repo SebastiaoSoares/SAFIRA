b563d00b52060348448643cc3ad824f9f2a5d051
Sebastião Sousa Soares <sebastiaosoares145@gmail.com>
2023-12-10 21:58:44 -0300

    Update recuperação

--- values.py
+++ values.py
@@ -4,13 +4,13 @@ r = []
 rSafe = ["success", "INCÊNDIO NÃO DETECTADO"]
 rNoSafe = ["danger", "INCÊNDIO DETECTADO!"]
 
-safe = True
+safe = False
 
 if safe:
     r = rSafe
 else:
     r = rNoSafe
 
-fResult = ["nivel1", "não detectada"]
+fResult = ["nivel1", "detectada"]
 tResult = ["nivel3", "48°C"]
 uResult = ["nivel2", "15% (baixa)"]
